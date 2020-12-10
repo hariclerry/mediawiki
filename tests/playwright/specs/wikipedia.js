0603d25b9cfedc4c6e6ecbc92f4af2d1a1638ee7
Harriet Ayugi <clerryha@gmail.com>
2020-12-10 16:16:19 +0300

    add assertion to playwright test

--- tests/playwright/specs/wikipedia.js
+++ tests/playwright/specs/wikipedia.js
@@ -1,8 +1,13 @@
-var { chromium } = require("playwright");
+let { chromium } = require("playwright");
+const assert = require("assert");
 
 (async () => {
 	const browser = await chromium.launch({ headless: false });
+	
 	const page = await browser.newPage();
 	await page.goto("https://www.wikipedia.org/");
+
+	const text = await page.innerText(".localized-slogan");
+	assert(text === "The Free Encyclopedia");
 	await browser.close();
 })();
