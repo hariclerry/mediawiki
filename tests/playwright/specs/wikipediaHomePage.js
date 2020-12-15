a6c4f44e8ada8e694fa7211a768aa44f5818849b
Harriet Ayugi <clerryha@gmail.com>
2020-12-15 16:29:39 +0300

    add video and screen recordings to playwright tests

--- tests/playwright/specs/wikipediaHomePage.js
+++ tests/playwright/specs/wikipediaHomePage.js
@@ -1,9 +1,16 @@
+const { saveVideo } = require( 'playwright-video' );
+
+let capture;
+
 describe( 'Page', () => {
 	beforeAll( async () => {
+		capture = await saveVideo( page, './tests/playwright/videos/recording.mp4' );
 		await page.goto( 'https://www.wikipedia.org/' );
+		await page.screenshot( { path: './tests/playwright/screenshots/screenshot.png' } );
 	} );
 
 	afterAll( async () => {
+		await capture.stop();
 		await browser.close();
 	} );
 
