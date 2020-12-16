55351b6cf8a91ac65bb48f36a3f8c61d11cc7dc5
Harriet Ayugi <clerryha@gmail.com>
2020-12-16 12:02:33 +0300

    modify test suite to include beforeEach and AfterEach hooks

--- tests/playwright/specs/wikipediaHomePage.js
+++ tests/playwright/specs/wikipediaHomePage.js
@@ -4,13 +4,27 @@ let capture;
 
 describe( 'Page', () => {
 	beforeAll( async () => {
-		capture = await saveVideo( page, './tests/playwright/videos/recording.mp4' );
+
 		await page.goto( 'https://www.wikipedia.org/' );
-		await page.screenshot( { path: './tests/playwright/screenshots/screenshot.png' } );
+
+	} );
+
+	beforeEach( async () => {
+		capture = await saveVideo(
+			page,
+			'./tests/playwright/videos/recording.mp4'
+		);
+		await page.screenshot( {
+			path: './tests/playwright/screenshots/screenshot.png'
+		} );
+	} );
+
+	afterEach( async () => {
+		await capture.stop();
 	} );
 
 	afterAll( async () => {
-		await capture.stop();
+
 		await browser.close();
 	} );
 
