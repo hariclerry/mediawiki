477412053b6844ec7c1ad930b9c2eed24826a27e
Harriet Ayugi <clerryha@gmail.com>
2020-12-21 05:02:27 +0300

    implement video recording and screenshots in playwright

--- tests/playwright/specs/wikipediaHomePage.js
+++ tests/playwright/specs/wikipediaHomePage.js
@@ -1,6 +1,24 @@
+const { saveVideo } = require( 'playwright-video' );
+
+let capture;
+
 describe( 'Page', () => {
 	beforeAll( async () => {
 		await page.goto( 'https://www.wikipedia.org/' );
+	} );
+
+	beforeEach( async () => {
+		capture = await saveVideo(
+			page,
+			'./tests/playwright/videos/recording.mp4'
+		);
+		await page.screenshot( {
+			path: './tests/playwright/screenshots/screenshot.png'
+		} );
+	} );
+
+	afterEach( async () => {
+		await capture.stop();
 	} );
 
 	afterAll( async () => {
