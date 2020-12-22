b809f6abd4373e442fcd2da92be6a29fd73aac95
Harriet Ayugi <clerryha@gmail.com>
2020-12-22 09:32:10 +0300

    move variable declarations inside the test scope

--- tests/playwright/specs/wikipediaHomePage.js
+++ tests/playwright/specs/wikipediaHomePage.js
@@ -2,10 +2,9 @@ const { saveVideo } = require( 'playwright-video' );
 const path = require( 'path' ),
 	LOGPATH = process.env.LOG_DIR || path.resolve( __dirname, '..', 'log' );
 
-let capture;
-
 describe( 'Page', () => {
 	it( 'should display sub title correctly', async () => {
+		let capture;
 		await page.goto( 'https://www.wikipedia.org/' );
 
 		capture = await saveVideo( page, `${LOGPATH}/recording.mp4` );
