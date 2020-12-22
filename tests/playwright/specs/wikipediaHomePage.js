1b5d93a1fbd57195eec73f66a9b0652b566c7505
Harriet Ayugi <clerryha@gmail.com>
2020-12-22 14:41:04 +0300

    rename variables and move video capture to top

--- tests/playwright/specs/wikipediaHomePage.js
+++ tests/playwright/specs/wikipediaHomePage.js
@@ -1,21 +1,20 @@
 const { saveVideo } = require( 'playwright-video' );
 const path = require( 'path' ),
-	LOGPATH = process.env.LOG_DIR || path.resolve( __dirname, '..', 'log' );
+	logPath = process.env.LOG_DIR || path.resolve( __dirname, '..', 'log' );
 
 describe( 'Page', () => {
 	it( 'should display sub title correctly', async () => {
-		let capture;
+		const captureVideo = await saveVideo( page, `${logPath}/recording.mp4` );
 		await page.goto( 'https://www.wikipedia.org/' );
-
-		capture = await saveVideo( page, `${LOGPATH}/recording.mp4` );
 		await page.screenshot( {
-			path: `${LOGPATH}/screenshot.png`
+			path: `${logPath}/screenshot.png`
 		} );
 
+		// eslint-disable-next-line one-var
 		const subTitleText = await page.innerText( '.localized-slogan' );
 		expect( subTitleText ).toEqual( 'The Free Encyclopedia' );
 
-		await capture.stop();
+		await captureVideo.stop();
 		await browser.close();
 	} );
 } );
