bee73df24c60db6ec3d8010fc56420de39b4abff
Harriet Ayugi <clerryha@gmail.com>
2020-12-28 16:20:22 +0300

    configure github actions to run docker

--- tests/playwright/specs/wikipediaHomePage.js
+++ tests/playwright/specs/wikipediaHomePage.js
@@ -1,14 +1,20 @@
+const { saveVideo } = require( 'playwright-video' );
+const path = require( 'path' ),
+	logPath = process.env.LOG_DIR || path.resolve( __dirname, '..', 'log' );
+
 describe( 'Page', () => {
-	beforeAll( async () => {
-		await page.goto( 'https://www.wikipedia.org/' );
-	} );
-
-	afterAll( async () => {
-		await browser.close();
-	} );
-
 	it( 'should display sub title correctly', async () => {
+		const captureVideo = await saveVideo( page, `${logPath}/recording.mp4` );
+		await page.goto( 'https://www.wikipedia.org/' );
+		await page.screenshot( {
+			path: `${logPath}/screenshot.png`
+		} );
+
+		// eslint-disable-next-line one-var
 		const subTitleText = await page.innerText( '.localized-slogan' );
 		expect( subTitleText ).toEqual( 'The Free Encyclopedia' );
+
+		await captureVideo.stop();
+		await browser.close();
 	} );
 } );
